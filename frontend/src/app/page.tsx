6e738b305310bcdea3fcf43e717a4e03cc6cd4fa
badmagick <uzairfarooqi@gmail.com>
2024-01-26 01:09:42 +0000

    other: setup vitest

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: 'App Router',
+};
+
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
